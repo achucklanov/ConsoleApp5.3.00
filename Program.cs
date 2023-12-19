0da307f6549d388c9b9cfa43d00acd22ffc2c1ff
AndreyChuck <a.chucklanov@yandex.ru>
2023-12-19 23:41:01 +0400

    Создание проекта нового

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ConsoleApp5._3._00
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
