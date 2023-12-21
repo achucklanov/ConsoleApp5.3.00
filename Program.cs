84663a75556de43d1117b2a0cc13f9ff8b760c75
AndreyChuck <a.chucklanov@yandex.ru>
2023-12-21 22:33:27 +0400

    Отладка 2 для работы с гит в терминале

--- Program.cs
+++ Program.cs
@@ -1,9 +1,16 @@
-﻿namespace ConsoleApp5._3._00
+﻿using System.Numerics;
+
+namespace ConsoleApp5._3._00
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Здравствуй, Мир!");
+
+            Console.WriteLine("Здравствуй, Мир!");
+            Console.WriteLine("Здравствуй, Мир!");
+
             var someName = "Евгения";
             Console.WriteLine(someName);
 
@@ -16,6 +23,8 @@
              //   Console.WriteLine("Введите ваше имя");
             //    name = Console.ReadLine();
             //}
+
+            
         }
         static void GetName(string name)
         {
